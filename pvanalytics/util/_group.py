21a6273ec92f3834acb50fdfc8e90919ac9bfac5
Will Vining <wfvining@gmail.com>
2020-08-21 11:16:50 -0600

    Add run_lengths to util._group
    Computing run-lenghts is a generally usefule function, beyond its use
    in the features.daytime module. I think it fits nicely in the
    util._group module since it is related to grouping data (a run is a
    group of consecutive equal values).

--- pvanalytics/util/_group.py
+++ pvanalytics/util/_group.py
@@ -25,3 +25,26 @@ def by_day(data):
     return data.groupby(
         pd.to_datetime(data.index.date).tz_localize(data.index.tz)
     )
+
+
+def run_lengths(series):
+    # Count the number of equal values adjacent to each value.
+    #
+    # Examples
+    # --------
+    # >>> _run_lengths(pd.Series([True, True, True]))
+    # 0    3
+    # 1    3
+    # 2    3
+    #
+    # >>> _run_lengths(
+    # ...     pd.Series([True, False, False, True, True, False]
+    # ... ))
+    # 0    1
+    # 1    2
+    # 2    2
+    # 3    2
+    # 4    2
+    # 5    1
+    runs = (series != series.shift(1)).cumsum()
+    return runs.groupby(runs).transform('count')
